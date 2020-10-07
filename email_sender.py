0cb9cdb95f54596e66cdb3f14db07143a5fbfb72
piotrzel <dzelo12@gmail.com>
2020-10-08 00:58:53 +0200

    new file

---
+++ email_sender.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Piotr Zel'
+email['to'] = 'dzelo12@gmail.com'
+email['subject'] = 'Odbierz nagrodę!!!'
+
+email.set_content(html.substitute({'name': 'Piotr'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587)as smtp:
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.login('piotrtestpy@gmail.com', 'Tygrys12')
+	smtp.send_message(email)
+	print('Wyszło!!!')
